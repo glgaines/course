4363bb565059a3528ac7fa513fe3284eda6a408c
Faunris <Faunris@gmail.com>
2012-05-28 18:40:57 +0600

    added chitozzz

--- settings.py
+++ settings.py
@@ -24,8 +24,8 @@ USE_TZ = True
 MEDIA_ROOT = '%s/public/media/' % PROJECT_DIR
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = 'http://dl.dropbox.com/u/15834380/static/'#'%s/public/static/' % PROJECT_DIR
-STATIC_URL = '/static/'
+STATIC_ROOT = '%s/public/static/' % PROJECT_DIR
+STATIC_URL = 'http://dl.dropbox.com/u/15834380/static/'
 
 STATICFILES_DIRS = (
 
